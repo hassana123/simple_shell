5d97b2e149a19bce689df5a27ea9aaaa4091efd2
Hassana <hassanaabdll1@gmail.com>
2023-07-21 06:10:25 -0700

    builtins

--- shell.h
+++ shell.h
@@ -5,19 +5,59 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* for command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+/* for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/* 1 if using system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+extern char **environ;
 
 /**
- * struct inbuilt - contains a builtin string and related function
- * @type: the inbuilt command flag
+ * struct builtin - contains a builtin string and related function
+ * @type: the builtin command flag
  * @func: the function
  */
-typedef struct inbuilt
+typedef struct builtin
 {
 	char *type;
 	int (*func)(info_t *);
-} inbuilt_table;
+} builtin_table;
 
-/*_atoi.c */
+/* _builtin.c */
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+
+/* _builtin1.c */
+int _myhistory(info_t *);
+int _myalias(info_t *);
+
+
+/* _atoi.c */
 int interactive(info_t *);
 int is_delim(char, char *);
 int _isalpha(int);
